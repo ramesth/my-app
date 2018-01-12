cea37cefe9f46a16d920f1e725294f23e0d20961
Ramesh Thapa <ramesth@hotmail.com>
2018-01-12 09:11:40 -0500

    d

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,7 +20,7 @@ export class AppComponent {
   name:String="Enter Name";
   age:Number=10;
   email:String="zzz@ssss.com";
-  app_url="https://angramtest.azurewebsites.net:443/";
+  app_url="https://angramtest.azurewebsites.net:443";
  
   persons:Person[];
   users:PersonProfile[];
